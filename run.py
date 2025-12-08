0771435267c24bde7edb5a0a46897b03c1b7a33c
maherjabeur <maherjabeur@gmail.com>
2025-12-08 15:09:24 +0100

    ss

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     # Lancer l'API
     uvicorn.run(
         "api:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True,
         log_level="info"
